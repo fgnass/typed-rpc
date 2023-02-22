6c9ec02c6104c5a24dd83c8a2c09ce0a45ab30c4
insraq <ruoysun@gmail.com>
2023-02-22 22:16:22 +0200

    Fix test

--- src/test/client.ts
+++ src/test/client.ts
@@ -1,24 +1,25 @@
+import "isomorphic-fetch";
+import tap from "tap";
 import { rpcClient, RpcError } from "../client.js";
 import { Service } from "./service.js";
-import tap from "tap";
-import "isomorphic-fetch";
 
 const apiUrl = process.env.SERVER_URL + "/api";
 
 tap.test("should talk to the server", async (t) => {
-  const client = rpcClient<Service>(apiUrl);
+  const client = rpcClient<Service>({ url: apiUrl });
   const result = await client.hello("world");
   t.equal(result, "Hello world!");
 });
 
 tap.test("should omit trailing undefined params", async (t) => {
-  const client = rpcClient<Service>(apiUrl);
+  const client = rpcClient<Service>({ url: apiUrl });
   const result = await client.greet("hello", undefined);
   t.equal(result, "hello world!");
 });
 
 tap.test("should override headers", async (t) => {
-  const client = rpcClient<Service>(apiUrl, {
+  const client = rpcClient<Service>({
+    url: apiUrl,
     getHeaders() {
       return {
         "Prefer-Status": "400",
@@ -30,22 +31,20 @@ tap.test("should override headers", async (t) => {
 });
 
 tap.test("should echo headers", async (t) => {
-  const client = rpcClient<Service>(
-    process.env.SERVER_URL + "/request-aware-api",
-    {
-      getHeaders() {
-        return {
-          "X-Hello": "world",
-        };
-      },
-    }
-  );
+  const client = rpcClient<Service>({
+    url: process.env.SERVER_URL + "/request-aware-api",
+    getHeaders() {
+      return {
+        "X-Hello": "world",
+      };
+    },
+  });
   const res = await client.echoHeader("X-Hello");
   t.equal(res, "world");
 });
 
 tap.test("should throw on errors", async (t) => {
-  const client = rpcClient<Service>(apiUrl);
+  const client = rpcClient<Service>({ url: apiUrl });
   const promise = client.sorry("Dave");
   t.rejects(promise, new RpcError("Sorry Dave.", -32000));
 });
