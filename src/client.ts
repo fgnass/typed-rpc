d94757453eaf4e3428a63854bf66864920d7f4ce
Peter Krauß <peter.krauss@kit.edu>
2023-07-23 16:28:37 +0200

    added support for superjson

--- src/client.ts
+++ src/client.ts
@@ -1,82 +1,91 @@
-export class RpcError extends Error {
-  code: number;
-  data?: unknown;
+import superjson from "superjson";
 
-  constructor(message: string, code: number, data?: unknown) {
-    super(message);
-    this.code = code;
-    this.data = data;
-    // https://www.typescriptlang.org/docs/handbook/2/classes.html#inheriting-built-in-types
-    Object.setPrototypeOf(this, RpcError.prototype);
-  }
+export class RpcError extends Error {
+	code: number;
+	data?: unknown;
+
+	constructor(message: string, code: number, data?: unknown) {
+		super(message);
+		this.code = code;
+		this.data = data;
+		// https://www.typescriptlang.org/docs/handbook/2/classes.html#inheriting-built-in-types
+		Object.setPrototypeOf(this, RpcError.prototype);
+	}
 }
 
 type RpcOptions = {
-  credentials?: RequestCredentials;
-  getHeaders?():
-    | Record<string, string>
-    | Promise<Record<string, string>>
-    | undefined;
+	credentials?: RequestCredentials;
+	getHeaders?():
+		| Record<string, string>
+		| Promise<Record<string, string>>
+		| undefined;
 };
 
 type Promisify<T> = T extends (...args: any[]) => Promise<any>
-  ? T // already a promise
-  : T extends (...args: infer A) => infer R
-  ? (...args: A) => Promise<R>
-  : T; // not a function;
+	? T // already a promise
+	: T extends (...args: infer A) => infer R
+	? (...args: A) => Promise<R>
+	: T; // not a function;
 
 type PromisifyMethods<T extends object> = {
-  [K in keyof T]: Promisify<T[K]>;
+	[K in keyof T]: Promisify<T[K]>;
 };
 
 export function rpcClient<T extends object>(url: string, options?: RpcOptions) {
-  const request = async (method: string, params: any[]) => {
-    const id = Date.now();
-    const headers = options?.getHeaders ? await options.getHeaders() : {};
+	const request = async (method: string, params: any[]) => {
+		const id = Date.now();
+		const headers = options?.getHeaders ? await options.getHeaders() : {};
+		const { json: paramJson, meta: paramMeta } = superjson.serialize(
+			removeTrailingUndefs(params)
+		);
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        ...headers,
-      },
-      body: JSON.stringify({
-        jsonrpc: "2.0",
-        id,
-        method,
-        params: removeTrailingUndefs(params),
-      }),
-      credentials: options?.credentials,
-    });
-    if (!res.ok) {
-      throw new RpcError(res.statusText, res.status);
-    }
-    const { result, error } = await res.json();
-    if (error) {
-      const { code, message, data } = error;
-      throw new RpcError(message, code, data);
-    }
-    return result;
-  };
+		const res = await fetch(url, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				...headers,
+			},
+			body: JSON.stringify({
+				jsonrpc: "2.0",
+				id,
+				method,
+				params: paramJson,
+				meta: paramMeta,
+			}),
+			credentials: options?.credentials,
+		});
+		if (!res.ok) {
+			throw new RpcError(res.statusText, res.status);
+		}
+		const { result, error, meta } = await res.json();
+		if (error) {
+			const { code, message, data } = error;
+			throw new RpcError(message, code, data);
+		}
+		if (meta) {
+			return superjson.deserialize({ json: result, meta });
+		}
+		return result;
+	};
 
-  return new Proxy(
-    {},
-    {
-      /* istanbul ignore next */
-      get(target, prop, receiver) {
-        if (typeof prop === "symbol") return;
-        if (prop.startsWith("$")) return;
-        if (prop in Object.prototype) return;
-        if (prop === "toJSON") return;
-        return (...args: any) => request(prop.toString(), args);
-      },
-    }
-  ) as PromisifyMethods<T>;
+	return new Proxy(
+		{},
+		{
+			/* istanbul ignore next */
+			get(target, prop, receiver) {
+				if (typeof prop === "symbol") return;
+				if (prop.startsWith("$")) return;
+				if (prop in Object.prototype) return;
+				if (prop === "toJSON") return;
+				return (...args: any) => request(prop.toString(), args);
+			},
+		}
+	) as PromisifyMethods<T>;
 }
 
 function removeTrailingUndefs(values: any[]) {
-  const a = [...values];
-  while (a.length && a[a.length - 1] === undefined) a.length--;
-  return a;
+	const a = [...values];
+	while (a.length && a[a.length - 1] === undefined) a.length--;
+	return a;
 }
--- src/server.ts
+++ src/server.ts
@@ -1,115 +1,133 @@
+import superjson from "superjson";
+
 export interface JsonRpcRequest {
-  jsonrpc: "2.0";
-  id?: string | number | null;
-  method: string;
-  params: any[];
+	jsonrpc: "2.0";
+	id?: string | number | null;
+	method: string;
+	params: any[];
+	meta?: any;
 }
 
 export interface JsonRpcResponse {
-  jsonrpc: "2.0";
-  id?: string | number | null;
+	jsonrpc: "2.0";
+	id?: string | number | null;
 }
 
 export interface JsonRpcErrorResponse extends JsonRpcResponse {
-  error: {
-    code: number;
-    message: string;
-    data?: any;
-  };
+	error: {
+		code: number;
+		message: string;
+		data?: any;
+	};
 }
 
 export interface JsonRpcSuccessResponse extends JsonRpcResponse {
-  result: any;
+	result: any;
+	meta?: any;
 }
 
 /**
  * Type guard to check if a given object is a valid JSON-RPC request.
  */
 export function isJsonRpcRequest(req: any): req is JsonRpcRequest {
-  if (req.jsonrpc !== "2.0") return false;
-  if (typeof req.method !== "string") return false;
-  if (!Array.isArray(req.params)) return false;
-  return true;
+	if (req.jsonrpc !== "2.0") return false;
+	if (typeof req.method !== "string") return false;
+	if (!Array.isArray(req.params)) return false;
+	return true;
 }
 
 /**
  * Type guard to check if an object has a certain property.
  */
 function hasProperty<T, P extends string>(
-  obj: T,
-  prop: P
+	obj: T,
+	prop: P
 ): obj is T & Record<P, unknown> {
-  return typeof obj === "object" && obj !== null && prop in obj;
+	return typeof obj === "object" && obj !== null && prop in obj;
 }
 
 /**
  * Type guard to check if an object has a certain method.
  */
 function hasMethod<T, P extends string>(
-  obj: T,
-  prop: P
+	obj: T,
+	prop: P
 ): obj is T & Record<P, (...params: any[]) => any> {
-  return hasProperty(obj, prop) && typeof obj[prop] === "function";
+	return hasProperty(obj, prop) && typeof obj[prop] === "function";
 }
 
 function getErrorCode(err: unknown) {
-  if (hasProperty(err, "code") && typeof err.code === "number") {
-    return err.code;
-  }
-  return -32000;
+	if (hasProperty(err, "code") && typeof err.code === "number") {
+		return err.code;
+	}
+	return -32000;
 }
 
 function getErrorMessage(err: unknown) {
-  if (hasProperty(err, "message") && typeof err.message === "string") {
-    return err.message;
-  }
-  return "";
+	if (hasProperty(err, "message") && typeof err.message === "string") {
+		return err.message;
+	}
+	return "";
 }
 
 /**
  * Returns the id or null if there is no valid id.
  */
 function getRequestId(req: unknown) {
-  if (hasProperty(req, "id")) {
-    const id = req.id;
-    if (typeof id === "string" || typeof id === "number") return id;
-  }
-  return null;
+	if (hasProperty(req, "id")) {
+		const id = req.id;
+		if (typeof id === "string" || typeof id === "number") return id;
+	}
+	return null;
 }
 
 export async function handleRpc(
-  request: JsonRpcRequest,
-  service: object
+	request: JsonRpcRequest,
+	service: object
 ): Promise<JsonRpcErrorResponse | JsonRpcSuccessResponse> {
-  const id = getRequestId(request);
-  if (!isJsonRpcRequest(request)) {
-    //The JSON sent is not a valid Request object
-    return {
-      jsonrpc: "2.0",
-      id,
-      error: { code: -32600, message: "Invalid Request" },
-    };
-  }
-  const { jsonrpc, method, params } = request;
-  if (!hasMethod(service, method)) {
-    console.log("Method %s not found", method, service);
-    return {
-      jsonrpc,
-      id,
-      error: { code: -32601, message: `Method not found: ${method}` },
-    };
-  }
-  try {
-    const result = await service[method](...params);
-    return { jsonrpc, id, result };
-  } catch (err) {
-    return {
-      jsonrpc,
-      id,
-      error: {
-        code: getErrorCode(err),
-        message: getErrorMessage(err),
-      },
-    };
-  }
+	const id = getRequestId(request);
+	if (!isJsonRpcRequest(request)) {
+		//The JSON sent is not a valid Request object
+		return {
+			jsonrpc: "2.0",
+			id,
+			error: { code: -32600, message: "Invalid Request" },
+		};
+	}
+	let { params } = request;
+	const { jsonrpc, method, meta } = request;
+	if (!hasMethod(service, method)) {
+		console.log("Method %s not found", method, service);
+		return {
+			jsonrpc,
+			id,
+			error: { code: -32601, message: `Method not found: ${method}` },
+		};
+	}
+	if (meta) {
+		const newParams = superjson.deserialize<any[]>({ json: params, meta });
+		if ( params.length !== newParams.length ) {
+			console.log("Could not deserialize request to method %s", method, service);
+			return {
+				jsonrpc,
+				id,
+				error: { code: -32602, message: `Could not deserialize request.` },
+			};
+		}
+		params = newParams
+	}
+	try {
+		const result = await service[method](...params);
+		const { json, meta } = superjson.serialize(result)
+		return { jsonrpc, id, result: json, meta };
+	} catch (err) {
+		return {
+			jsonrpc,
+			id,
+			error: {
+				code: getErrorCode(err),
+				message: getErrorMessage(err),
+			},
+		};
+	}
 }
